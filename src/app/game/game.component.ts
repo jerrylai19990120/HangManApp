941fd1fbe7b6d08b62cd204567d57a1b48d48256
jerry <jerrylai19990120@gmail.com>
2020-06-29 12:57:40 -0400

    hang man drawing updates

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,7 +12,7 @@ export class GameComponent implements OnInit {
   public displayWord = '';
   public correctLetters = '';
   public incorrectLetters = '';
-  public guesses = 6;
+  public guesses = 10;
   public lettersLeft = 0;
   public status = '';
 
@@ -20,6 +20,119 @@ export class GameComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  head(): void {
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2'
+    context.arc(150, 100, 22, 0, 2 * Math.PI);
+    context.stroke();
+  }
+
+  base(): void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '6'
+    context.moveTo(46,330);
+    context.lineTo(226, 330);
+    context.stroke();
+  }
+
+  hangBar(): void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '6';
+    context.moveTo(66,30);
+    context.lineTo(66, 330);
+    context.stroke();
+  }
+
+  bar():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '6';
+    context.moveTo(66,40);
+    context.lineTo(206, 40);
+    context.stroke();
+  }
+
+  rope():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '6';
+    context.moveTo(150, 40);
+    context.lineTo(150, 78);
+    context.stroke();
+
+  }
+
+  body():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2';
+    context.moveTo(150, 123);
+    context.lineTo(150, 208);
+    context.stroke();
+  }
+
+  leftArm():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2';
+    context.moveTo(150, 142);
+    context.lineTo(90, 154);
+    context.stroke();
+  }
+
+  rightArm():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2';
+    context.moveTo(150, 142);
+    context.lineTo(210, 154);
+    context.stroke();
+  }
+
+  leftLeg():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2';
+    context.moveTo(150, 206);
+    context.lineTo(90, 266);
+    context.stroke();
+
+  }
+
+  rightLeg():void{
+    let canvas: any = document.getElementById('drawings');
+    let context: any = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = '2';
+    context.moveTo(150, 206);
+    context.lineTo(210, 266);
+    context.stroke();
+  }
+
+
 
   generate(input): void{
     this.word = input.value;
@@ -43,6 +156,27 @@ export class GameComponent implements OnInit {
       input.value = '';
     }else{
       this.guesses--;
+      if(this.guesses===9){
+        this.base();
+      }else if(this.guesses===8){
+        this.hangBar();
+      }else if(this.guesses===7){
+        this.bar();
+      }else if(this.guesses===6){
+        this.rope();
+      }else if(this.guesses===5){
+        this.head();
+      }else if(this.guesses===4){
+        this.body();
+      }else if(this.guesses===3){
+        this.leftArm();
+      }else if(this.guesses===2){
+        this.rightArm();
+      }else if(this.guesses===1){
+        this.leftLeg();
+      }else if(this.guesses===0){
+        this.rightLeg();
+      }
       this.incorrectLetters += letter;
       input.value = '';
     }
